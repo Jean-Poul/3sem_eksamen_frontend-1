0e0024a4cb98b0c36c00ff2a11d299deb596d6e3
Mick <grevmivlos@gmail.com>
2020-12-01 10:31:55 +0100

    Code formatting

--- src/LaunchInformation.js
+++ src/LaunchInformation.js
@@ -1,96 +1,126 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 import "./ca3.css";
 import { Form, Container, Row, Col, Button } from "react-bootstrap";
-import Comments, { WriteComment } from './Comments';
-import LaunchString, {date,launchtime, location, provlogo,launchServiceProviderName, provtype, missionname, missiondetails,missiontype ,rocket,rocketimg}  from "./LaunchString";
-
+import Comments, { WriteComment } from "./Comments";
+import LaunchString, {
+  date,
+  launchtime,
+  location,
+  provlogo,
+  launchServiceProviderName,
+  provtype,
+  missionname,
+  missiondetails,
+  missiontype,
+  rocket,
+  rocketimg,
+} from "./LaunchString";
 
 const LaunchInformation = () => {
-    return (
-        <div >
-            <LaunchString/>
-            <Container className="">
-                <Row className="ca3siteBody">
+  return (
+    <div>
+      <LaunchString />
+      <Container className="">
+        <Row className="ca3siteBody">
+          <Col sm={4}>
+            <div className="ca3h1">
+              Date: <br />
+              Estimated Launch:
+              <br />
+              Location:
+              <br />
+            </div>
+          </Col>
+          <Col sm={8}>
+            <div className="ca3P">
+              {date} <br />
+              {launchtime}
+              <br />
+              {location}
+              <br />
+            </div>
+          </Col>
+        </Row>
 
-                    <Col sm={4}>
-                        <div className="ca3h1">
-                            Date: <br />
-                            Estimated Launch:<br />
-                            Location:<br />
-                        </div>
-                    </Col>
-                    <Col sm={8}>
-                        <div className="ca3P">
-                        {date} <br/>
-                        {launchtime}<br/>
-                        {location}<br/>
-                        </div>
-                    </Col>
-                </Row>
+        <Col>
+          <br />
+          <img
+            src="https://spacelaunchnow-prod-east.nyc3.digitaloceanspaces.com/media/logo/spacex_logo_20191121063502.png"
+            className="img"
+          ></img>
+          <br />
+          <br />
+        </Col>
+        <Row className="ca3siteBody">
+          <Col sm={4}>
+            <div className="ca3h1">
+              Launch Provider:
+              <br />
+              Provider Type:
+              <br />
+              <br />
+              Mission Name:
+              <br />
+              Mission Type:
+              <br />
+              Mission Details:
+              <br />
+            </div>
+          </Col>
 
-                <Col>
-                    <br />
-                    <img src="https://spacelaunchnow-prod-east.nyc3.digitaloceanspaces.com/media/logo/spacex_logo_20191121063502.png" className="img"></img>
-                    <br />
-                    <br />
-                </Col>
-                <Row className="ca3siteBody">
-
-                <Col sm={4}>
-                    <div className="ca3h1">
-                        Launch Provider:<br/>
-                        Provider Type:<br/><br/>
-                        Mission Name:<br/>
-                        Mission Type:<br/>
-                        Mission Details:<br/>
-                    </div>
-                </Col>
-
-                <Col sm={8}>
-                    <div className="ca3P">
-                        {launchServiceProviderName}<br/>
-                        {provtype}<br/><br/>
-                        {missionname}<br/>
-                        {missiontype}<br/>
-                        
-                        
-                    </div>
-                    <div className="ca3MDetails">
-                    {missiondetails}<br/>
-                    </div>
-                        <br />
-                    </Col>
-
-                <Col sm={4}>
-                    <div className="ca3h1">
-                        Rocket:<br/>
-                    </div>
-                </Col>
-
-                <Col sm={8}>
-                    <div className="ca3P">
-                        {rocket}<br/>
-
-                </Row>
-                <Col>
-                    <br />
-                    <img src="https://spacelaunchnow-prod-east.nyc3.digitaloceanspaces.com/media/agency_images/arianespace_image_20190207032425.jpeg" className="imgRocket"></img>
-                    <br />
-                    <br />
-                </Col>
-            </Container>
-
-            <hr className="ca3Hr" />
+          <Col sm={8}>
+            <div className="ca3P">
+              {launchServiceProviderName}
+              <br />
+              {provtype}
+              <br />
+              <br />
+              {missionname}
+              <br />
+              {missiontype}
+              <br />
+            </div>
+            <div className="ca3MDetails">
+              {missiondetails}
+              <br />
+            </div>
             <br />
-            <div className="ca3LaunchSmall">Comment Section</div>
-            <Container>
-                <Comments />
-                <WriteComment />
-            </Container>
+          </Col>
 
-        </div>
-    );
+          <Col sm={4}>
+            <div className="ca3h1">
+              Rocket:
+              <br />
+            </div>
+          </Col>
+
+          <Col sm={8}>
+            <div className="ca3P">
+              {rocket}
+              <br />
+            </div>
+          </Col>
+        </Row>
+        <Col>
+          <br />
+          <img
+            src="https://spacelaunchnow-prod-east.nyc3.digitaloceanspaces.com/media/agency_images/arianespace_image_20190207032425.jpeg"
+            className="imgRocket"
+          ></img>
+          <br />
+          <br />
+        </Col>
+      </Container>
+
+      <hr className="ca3Hr" />
+      <br />
+      <div className="ca3LaunchSmall">Comment Section</div>
+      <Container>
+        <Comments />
+        <WriteComment />
+      </Container>
+    </div>
+  );
 };
-
 
 export default LaunchInformation;
--- src/LaunchString.js
+++ src/LaunchString.js
@@ -1,9 +1,7 @@
-import React, { useState, useEffect } from "react"
+import React, { useState, useEffect } from "react";
 import "./ca3.css";
 import { URLLaunchString } from "./settings";
 import { Form, Container, Row, Col, Button } from "react-bootstrap";
-
-
 
 export let launchServiceProviderName;
 export let location;
@@ -22,13 +20,9 @@ export let missionname;
 export let missiondetails;
 export let missiontype;
 export let rocket;
-export let rocketimg
+export let rocketimg;
 
-
-
-const LaunchString =() => {
-  
-  
+const LaunchString = () => {
   const [launchString, setLaunchString] = useState([]);
 
   // loads launchString first time
@@ -45,13 +39,13 @@ const LaunchString =() => {
   };
 
   let launchNo = 2;
- 
 
   if (JSON.stringify(launchString).length > 100) {
-    launchServiceProviderName = launchString.results[launchNo].launch_service_provider.name;
+    launchServiceProviderName =
+      launchString.results[launchNo].launch_service_provider.name;
     location = launchString.results[launchNo].pad.location.name;
     countrycode = launchString.results[launchNo].pad.location.country_code;
-    
+
     wikilink = launchString.results[launchNo].pad.url.wiki_url;
     latitude = launchString.results[launchNo].pad.latitude;
     longitude = launchString.results[launchNo].pad.longitude;
@@ -59,23 +53,25 @@ const LaunchString =() => {
     date = launchString.results[launchNo].net;
     launchtime = launchString.results[launchNo].net;
     provwiki = launchString.results[launchNo].launch_service_provider.url.wiki;
-    provlogo = launchString.results[launchNo].launch_service_provider.url.logo_url;
+    provlogo =
+      launchString.results[launchNo].launch_service_provider.url.logo_url;
     provtype = launchString.results[launchNo].launch_service_provider.type;
     missiontype = launchString.results[launchNo].mission.type;
     missionname = launchString.results[launchNo].mission.name;
     missiondetails = launchString.results[launchNo].mission.description;
     rocket = launchString.results[launchNo].rocket.configuration.full_name;
-    rocketimg = launchString.results[launchNo].rocket.configuration.url.description;
+    rocketimg =
+      launchString.results[launchNo].rocket.configuration.url.description;
   }
 
-  console.log("launch service provider:" +launchServiceProviderName);
-  console.log("location " +location);
+  console.log("launch service provider:" + launchServiceProviderName);
+  console.log("location " + location);
   console.log("countrycode" + countrycode);
   console.log("wikilink " + wikilink);
   console.log("lattitude " + latitude);
   console.log("longitude " + longitude);
   console.log("date " + date);
-  console.log("launchtime "+ launchtime);
+  console.log("launchtime " + launchtime);
   console.log("provlogo " + provlogo);
   console.log("provtype " + provtype);
   console.log("missionname " + missionname);
@@ -83,12 +79,8 @@ const LaunchString =() => {
   console.log("missiontype " + missiontype);
   console.log("rocket " + rocket);
   console.log("rocketimg " + rocketimg);
-  
 
-  return (
-          <div></div>
-    );
-  };
-  
+  return <div></div>;
+};
 
-  export default LaunchString;
+export default LaunchString;
