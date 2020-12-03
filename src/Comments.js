6f74b204249a7a25f68fe07bf3d84031b00cbc0d
Jean-Poul <cph-jl360@cphbusiness.dk>
2020-12-03 14:55:59 +0100

    Comments with a user

--- src/Comments.js
+++ src/Comments.js
@@ -1,98 +1,105 @@
-import React, { useState, useEffect } from "react"
+import React, { useState, useEffect } from "react";
 import "./ca3.css";
-import { Form, Container, Row, Col, Button } from "react-bootstrap";
+import { Row, Col, Button } from "react-bootstrap";
 import { URLComments } from "./settings";
 
-
 const Comments = () => {
+  const [comments, setComments] = useState({});
 
-    const [comments, setComments] = useState({});
+  const fetchComments = () => {
+    fetch(URLComments)
+      .then((res) => res.json())
+      .then((data) => {
+        setComments(data);
+        console.log("FETCH" + JSON.stringify(data));
+      });
+  };
 
-    const fetchComments = () => {
-        fetch(URLComments)
-            .then(res => res.json())
-            .then(data => {
-                setComments(data);
-                // console.log(data);
-            });
-    }
+  useEffect(() => {
+    fetchComments();
+  }, []);
 
-    useEffect(() => {
-        fetchComments();
-    }, []);
-
-    return (
-        <div>
-            <div>
-                <Row>
-                    <Col sm={2}></Col>
-                    <Col sm={8}>
-                        {(typeof comments.all != "undefined") ? (
-                            <div>
-                                {comments.all.map(data =>
-                                    <div key={data.id}>
-                                        <Row>
-                                            <div>
-                                                <div className="ca3CommentName">Blastoff34 {data.userName}
-                                                &nbsp;
-                                                <div className="ca3CommentInfo">Commented @ {data.created}</div>
-                                                </div>
-                                            </div>
-                                        </Row>
-                                        {/* <div className="ca3Comment">Id:{data.id} &nbsp;</div> */}
-                                        <div className="ca3Comment">{data.userComment} &nbsp;</div>
-                                        <br />
-                                    </div>
-                                )}
-                            </div>
-                        ) : ('')}
-                        <br />  <br /><br />
-                    </Col>
-                    <Col sm={2}></Col>
-                </Row>
-            </div>
-        </div>
-    );
+  return (
+    <div>
+      <div>
+        <Row>
+          <Col sm={2}></Col>
+          <Col sm={8}>
+            {typeof comments.all != "undefined" ? (
+              <div>
+                {comments.all.map((data) => (
+                  <div key={data.id}>
+                    <Row>
+                      <div>
+                        <div className="ca3CommentName">
+                          Blastoff34 {data.userName}
+                          {console.log(data)}
+                          &nbsp;
+                          <div className="ca3CommentInfo">
+                            Commented @ {data.created}
+                          </div>
+                        </div>
+                      </div>
+                    </Row>
+                    {/* <div className="ca3Comment">Id:{data.id} &nbsp;</div> */}
+                    <div className="ca3Comment">{data.userComment} &nbsp;</div>
+                    <br />
+                  </div>
+                ))}
+              </div>
+            ) : (
+              ""
+            )}
+            <br /> <br />
+            <br />
+          </Col>
+          <Col sm={2}></Col>
+        </Row>
+      </div>
+    </div>
+  );
 };
-
-
 
 export const WriteComment = () => {
+  const init = { username: "", comment: "" };
+  const [newComment, setNewComment] = useState(init);
 
-    const init = { username: "", comment: "" };
-    const [newComment, setNewComment] = useState(init);
+  const submitComment = (evt) => {
+    evt.preventDefault();
+    WriteComment(newComment.username, newComment.comment);
+  };
+  const onChange = (evt) => {
+    setNewComment({ ...newComment, [evt.target.id]: evt.target.value });
+  };
 
-    const submitComment = (evt) => {
-        evt.preventDefault();
-        WriteComment(newComment.username, newComment.comment);
-    }
-    const onChange = (evt) => {
-        setNewComment({ ...newComment, [evt.target.id]: evt.target.value })
-    }
-
-    return (
-        <div>
-
-            <Row>
-                <Col sm={2}></Col>
-                <Col sm={8}>
-                    <div class="col-xs-4">
-
-                        <textarea class="form-control" id="Comment" rows="5" placeholder="Enter username"></textarea>
-                    </div>
-                    <div className="ca3Submit">
-
-                        <Button onClick={submitComment} class="btn btn-link submit" variant="primary" type="submit">
-                            Submit
-                </Button>
-                    </div>
-                </Col>
-                <Col sm={2}></Col>
-            </Row>
-
-        </div>
-    );
+  return (
+    <div>
+      <Row>
+        <Col sm={2}></Col>
+        <Col sm={8}>
+          <div class="col-xs-4">
+            <textarea
+              class="form-control"
+              id="Comment"
+              rows="5"
+              placeholder="Enter username"
+            ></textarea>
+          </div>
+          <div className="ca3Submit">
+            <Button
+              onClick={submitComment}
+              class="btn btn-link submit"
+              variant="primary"
+              type="submit"
+            >
+              Submit
+            </Button>
+          </div>
+        </Col>
+        <Col sm={2}></Col>
+      </Row>
+    </div>
+  );
 };
-
 
 export default Comments;
