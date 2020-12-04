08182ac44219f1eb49acee208b81c97284fd3677
Alexander Pihl <cph-as509@cphbusiness.dk>
2020-12-04 10:01:41 +0100

    comments works but still without user relation and rocket relation

--- src/Comments.js
+++ src/Comments.js
@@ -21,17 +21,8 @@ const Comments = () => {
             });
     }
 
-    const fetchNewComments = () => {
-        fetch(URLComments + "all")
-            .then(res => res.json())
-            .then(data => {
-                setComments(data);
-            });
-    }
-
     const submitComment = (evt) => {
         evt.preventDefault();
-        //WriteComment(newComment.username, newComment.comment);
 
         let options = {
             method: "POST",
@@ -44,9 +35,7 @@ const Comments = () => {
             })
         }
         fetch(URLComments, options);
-        fetchNewComments();
     }
-
 
     return (
         <div>
@@ -79,12 +68,11 @@ const Comments = () => {
                 </Row>
             </div>
             <div>
-
                 <Row>
                     <Col sm={2}></Col>
                     <Col sm={8}>
                         <div class="col-xs-4">
-                            <Form onChange={onChange} className="mt-4" label="">
+                            <Form onChange={fetchAllComments()} className="mt-4" label="">
                                 <textarea
                                     class="form-control"
                                     rows="5"
@@ -107,28 +95,5 @@ const Comments = () => {
         </div>
     );
 };
-
-/* function makeOptions(method, body) {
-    const opts = {
-        method: method,
-        headers: {
-            "Content-type": "application/json",
-            "Accept": "application/json"
-        }
-    }
-    if (body) {
-        opts.body = JSON.stringify(body);
-    }
-    return opts;
-} */
-
-
-//const init = { username: "", comment: "" };
-//const [newComment, setNewComment] = useState(init);
-
-const onChange = (evt) => {
-    //setNewComment({ ...newComment, [evt.target.id]: evt.target.value })
-}
-
 
 export default Comments;
