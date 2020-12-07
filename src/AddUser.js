95c8d652abe7001e29b4a7195d82b07df3dfdeb5
Jean-Poul <cph-jl360@cphbusiness.dk>
2020-12-07 15:25:40 +0100

    User kan nu oprettes med en user role og pass bliver saltet

--- src/AddUser.js
+++ src/AddUser.js
@@ -1,62 +1,56 @@
 import { Form, Container, Row, Col, Button } from "react-bootstrap";
+import { URL } from './settings';
 
 const AddUser = () => {
-
   const perfomAddUser = (evt) => {
     evt.preventDefault();
 
     let options = {
       method: "POST",
       headers: {
-        'Accept': 'application/json',
-        'Content-Type': 'application/json'
+        Accept: "application/json",
+        "Content-Type": "application/json",
       },
       body: JSON.stringify({
-        userName: document.getElementById("userName").value,
-        password: document.getElementById("password").value
+        userID: document.getElementById("userName").value,
+        password: document.getElementById("userPassword").value
       })
-    }
-    fetch(URL + "/user", options);
-
+    };
+    fetch(URL + "/api/users", options);
 
     console.log("User added");
-    document.getElementById("userSubmitted").innerHTML = <div class="alert alert-success" role="alert">You've created a user, login above.</div>;
-  }
-
+    document.getElementById("userSubmitted").innerHTML =
+      '<div class="alert alert-success" role="alert">You\'ve created a user, login above.</div>';
+  };
 
   return (
     <div>
-
       <Container>
         <Row>
-          <Col>
-          </Col>
+          <Col></Col>
           <Col>
             <h2 className="ca3White">Sign up</h2>
             <Form className="mt-4">
               <Form.Group controlId="userName">
-
                 <div id="userSubmitted" />
 
                 <Form.Label className="ca3White">Username</Form.Label>
                 <Form.Control type="text" placeholder="Enter username" />
               </Form.Group>
-              <Form.Group controlId="password">
+              <Form.Group controlId="userPassword">
                 <Form.Label className="ca3White">Password</Form.Label>
                 <Form.Control type="password" placeholder="Enter password" />
               </Form.Group>
               <Button onClick={perfomAddUser} variant="primary" type="submit">
                 Submit
-                </Button>
+              </Button>
             </Form>
           </Col>
-          <Col>
-          </Col>
+          <Col></Col>
         </Row>
       </Container>
     </div>
-  )
-
-}
+  );
+};
 
 export default AddUser;
