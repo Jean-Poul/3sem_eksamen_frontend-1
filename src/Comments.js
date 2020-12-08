600404e3bdc3012fa343cb447003729307d69a25
Jean-Poul <cph-jl360@cphbusiness.dk>
2020-12-08 09:40:12 +0100

    binding comment to user

---
+++ src/Comments.js
@@ -0,0 +1,115 @@
+import React, { useState, useEffect } from "react"
+import "./ca3.css";
+import { Form,  Row, Col, Button } from "react-bootstrap";
+import { URL } from "./settings";
+import facade from "./apiFacade";
+
+
+const Comments = (props) => {
+
+  function parseJwtName(name) {
+    let tokenName = JSON.parse(atob(name.split('.')[1]));
+    return tokenName.username;
+  }
+
+    const [comments, setComments] = useState([]);
+
+    useEffect(() => {
+        fetchAllComments();
+    }, []);
+
+    const fetchAllComments = () => {
+        fetch(URL + "/api/comments/all")
+            .then(res => res.json())
+            .then(data => {
+                setComments(data);
+                console.log(data)
+            });
+    }
+
+    const submitComment = (evt) => {
+        evt.preventDefault();
+
+
+
+        let options = {
+            method: "POST",
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                userComment: document.getElementById("Comment").value,
+                rocketID: props.rocketID,
+                //userID: parseJwtName(facade.getToken())
+            })
+        }
+        fetch(URL + "/api/comments", options)
+        .then(res => {
+          fetchAllComments();
+        });  
+    }
+
+    return (
+        <div>
+            <div>
+
+                <Row>
+                    <Col sm={2}></Col>
+                    <Col sm={8}>
+                        {(typeof comments.all != "undefined") ? (
+                            <div>
+                                {comments.all.map(data =>
+                                    <div key={data.id}>
+                                        <Row>
+                                            <div>
+                                                <div className="ca3CommentName">Blastoff34 {data.USER_user_name}
+                                                &nbsp; 
+                                                <div className="ca3CommentInfo">Commented @ {data.created}</div>
+                                                </div>
+                                                <div className="ca3CommentInfo">ROCKET ID:  {data.rocketID}
+                                                &nbsp;
+                                                </div>
+                                            </div>
+                                        </Row>
+                                        <div className="ca3Comment">{data.userComment} &nbsp;</div>
+                                        <br />
+                                    </div>
+                                )}
+                            </div>
+                        ) : ('')}
+                        <br />  <br /><br />
+                    </Col>
+                    <Col sm={2}></Col>
+                </Row>
+            </div>
+            <div>
+                <Row>
+                    <Col sm={2}></Col>
+                    <Col sm={8}>
+                        <div class="col-xs-4">
+                            <Form className="mt-4" label="">
+                                <textarea
+                                    class="form-control"
+                                    rows="5"
+                                    type="text"
+                                    id="Comment"
+                                    placeholder="Write a comment"
+                                />
+                                <div className="ca3Submit">
+                                    <Button onClick={submitComment} variant="primary" type="submit">
+                                        Submit your comment
+                                    </Button>
+                                </div>
+                            </Form>
+                        </div>
+                    </Col>
+                    <Col sm={2}></Col>
+                </Row>
+
+            </div>
+        </div>
+    );
+};
+
+export default Comments;
--- src/Informationpage.js
+++ src/Informationpage.js
@@ -1,107 +1,158 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 import { NavLink } from "react-router-dom";
 import { Nav, Navbar, Container, Row, Col } from "react-bootstrap";
-import rocketlaunchlogo from "./assets/rocketlaunch_logo_orange_dark_1500.png"
-import { useLocation, useHistory } from 'react-router-dom'
+import rocketlaunchlogo from "./assets/rocketlaunch_logo_orange_dark_1500.png";
+import { useLocation, useHistory } from "react-router-dom";
 import { URL } from "./settings";
 import Countdown from "./Countdown";
+import Comments from "./Comments";
 
 function InformationPage() {
   const [nextlaunch, setNextlaunch] = useState({});
   const [launchprovider, setLaunchprovider] = useState({});
   let location = useLocation();
   let history = useHistory();
-  let index = (location.launchProp !== undefined) ? location.launchProp.index : history.push("/");
+  let index =
+    location.launchProp !== undefined
+      ? location.launchProp.index
+      : history.push("/");
   console.log(index);
 
   const fetchNextlaunch = () => {
     fetch(`${URL}/api/nextlaunch/upcoming`)
-      .then(res => res.json())
-      .then(data => {
+      .then((res) => res.json())
+      .then((data) => {
         setNextlaunch(data);
         console.log(data);
-        
-        let i = (index !== undefined) ? index : 0;
-        
+
+        let i = index !== undefined ? index : 0;
+
         fetch(data.results[i].launch_service_provider.url)
-          .then(result => result.json())
-          .then(data2 => {
+          .then((result) => result.json())
+          .then((data2) => {
             setLaunchprovider(data2);
             console.log(data2);
           });
       });
-  }
+  };
 
   useEffect(() => {
     fetchNextlaunch();
   }, []);
 
-
   const dateBuilder = (timestamp) => {
     let d = new Date(timestamp);
-    let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
-    let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+    let months = [
+      "January",
+      "February",
+      "March",
+      "April",
+      "May",
+      "June",
+      "July",
+      "August",
+      "September",
+      "October",
+      "November",
+      "December"
+    ];
+    let days = [
+      "Sunday",
+      "Monday",
+      "Tuesday",
+      "Wednesday",
+      "Thursday",
+      "Friday",
+      "Saturday"
+    ];
 
     let day = days[d.getDay()];
     let date = d.getDate();
     let month = months[d.getMonth()];
     let year = d.getFullYear();
 
-    return `${day}, ${date} ${month} ${year}`
-  }
+    return `${day}, ${date} ${month} ${year}`;
+  };
 
   const timeBuilder = (timestamp) => {
     let d = new Date(timestamp);
 
-    let hours = (d.getHours() < 10 ? '0' : '') + d.getHours();
-    let minutes = (d.getMinutes() < 10 ? '0' : '') + d.getMinutes();
-    let seconds = (d.getSeconds() < 10 ? '0' : '') + d.getSeconds();
+    let hours = (d.getHours() < 10 ? "0" : "") + d.getHours();
+    let minutes = (d.getMinutes() < 10 ? "0" : "") + d.getMinutes();
+    let seconds = (d.getSeconds() < 10 ? "0" : "") + d.getSeconds();
 
-    return `${hours}:${minutes}:${seconds}`
-  }
+    return `${hours}:${minutes}:${seconds}`;
+  };
 
   return (
     <div>
-      {(typeof nextlaunch.results != "undefined") ? (
+      {typeof nextlaunch.results != "undefined" ? (
         <div>
           <div>
-            <img src={rocketlaunchlogo} className="imgLogo" alt="" /><br />
+            <img src={rocketlaunchlogo} className="imgLogo" alt="" />
+            <br />
             <div className="ca3LaunchBig ca3White">Launch information</div>
-            <div className="ca3CDBig"><Countdown date={(nextlaunch.results[index].net)} /></div>
-            <div className="ca3LaunchinfoBig ca3White">{nextlaunch.results[index].launch_service_provider.name}</div>
-            <div className="ca3LaunchinfoBig ca3White">{nextlaunch.results[index].pad.location.name}</div>
-            <div className="ca3LaunchinfoBig ca3White">{nextlaunch.results[index].pad.location.country_code}</div>
+            <div className="ca3CDBig">
+              <Countdown date={nextlaunch.results[index].net} />
+            </div>
+            <div className="ca3LaunchinfoBig ca3White">
+              {nextlaunch.results[index].launch_service_provider.name}
+            </div>
+            <div className="ca3LaunchinfoBig ca3White">
+              {nextlaunch.results[index].pad.location.name}
+            </div>
+            <div className="ca3LaunchinfoBig ca3White">
+              {nextlaunch.results[index].pad.location.country_code}
+            </div>
 
             <div>
               <Navbar variant="dark">
                 <Nav className="ca3NavbarBig m-auto">
-                  <NavLink className="nav-link ca3NavbarBig ca3Orange" exact activeClassName="selected" href="/" to="/">
+                  <NavLink
+                    className="nav-link ca3NavbarBig ca3Orange"
+                    exact
+                    activeClassName="selected"
+                    href="/"
+                    to="/"
+                  >
                     Home
                   </NavLink>
-                  <NavLink className="nav-link ca3NavbarBig ca3Orange" activeClassName="selected" to={{
-                    pathname: "/information",
-                    launchProp: { index: index }
-                  }}>
+                  <NavLink
+                    className="nav-link ca3NavbarBig ca3Orange"
+                    activeClassName="selected"
+                    to={{
+                      pathname: "/information",
+                      launchProp: { index: index }
+                    }}
+                  >
                     Information
                   </NavLink>
-                  <NavLink className="nav-link ca3NavbarBig ca3Orange" activeClassName="selected" to={{
-                    pathname: "/location",
-                    launchProp: {
-                      index: index,
-                      lat: nextlaunch.results[index].pad.latitude,
-                      lon: nextlaunch.results[index].pad.longitude
-                    }
-                  }}>
+                  <NavLink
+                    className="nav-link ca3NavbarBig ca3Orange"
+                    activeClassName="selected"
+                    to={{
+                      pathname: "/location",
+                      launchProp: {
+                        index: index,
+                        lat: nextlaunch.results[index].pad.latitude,
+                        lon: nextlaunch.results[index].pad.longitude
+                      }
+                    }}
+                  >
                     Location
                   </NavLink>
-                  <NavLink className="nav-link ca3NavbarBig ca3Orange" activeClassName="selected" to={{
-                    pathname: "/weather",
-                    launchProp: {
-                      index: index,
-                      lat: nextlaunch.results[index].pad.latitude,
-                      lon: nextlaunch.results[index].pad.longitude
-                    }
-                  }}>
+                  <NavLink
+                    className="nav-link ca3NavbarBig ca3Orange"
+                    activeClassName="selected"
+                    to={{
+                      pathname: "/weather",
+                      launchProp: {
+                        index: index,
+                        lat: nextlaunch.results[index].pad.latitude,
+                        lon: nextlaunch.results[index].pad.longitude
+                      }
+                    }}
+                  >
                     Weather
                   </NavLink>
                 </Nav>
@@ -111,8 +162,12 @@ function InformationPage() {
           <div>
             <Container>
               <Row>
-                <Col sm={4}><div className="ca3P">&nbsp;</div></Col>
-                <Col sm={8}><div className="ca3P ca3White">&nbsp;</div></Col>
+                <Col sm={4}>
+                  <div className="ca3P">&nbsp;</div>
+                </Col>
+                <Col sm={8}>
+                  <div className="ca3P ca3White">&nbsp;</div>
+                </Col>
               </Row>
 
               <Row>
@@ -120,7 +175,9 @@ function InformationPage() {
                   <div className="ca3h1 ca3Orange">Date:</div>
                 </Col>
                 <Col sm={8}>
-                  <div className="ca3P ca3White">{dateBuilder(nextlaunch.results[index].net)}</div>
+                  <div className="ca3P ca3White">
+                    {dateBuilder(nextlaunch.results[index].net)}
+                  </div>
                 </Col>
               </Row>
               <Row>
@@ -128,7 +185,9 @@ function InformationPage() {
                   <div className="ca3h1 ca3Orange">Estimated Launch:</div>
                 </Col>
                 <Col sm={8}>
-                  <div className="ca3P ca3White">{timeBuilder(nextlaunch.results[index].net)}</div>
+                  <div className="ca3P ca3White">
+                    {timeBuilder(nextlaunch.results[index].net)}
+                  </div>
                 </Col>
               </Row>
               <Row>
@@ -136,7 +195,9 @@ function InformationPage() {
                   <div className="ca3h1 ca3Orange">Location:</div>
                 </Col>
                 <Col sm={8}>
-                  <div className="ca3P ca3White">{nextlaunch.results[index].pad.location.name}</div>
+                  <div className="ca3P ca3White">
+                    {nextlaunch.results[index].pad.location.name}
+                  </div>
                 </Col>
               </Row>
               <Row>
@@ -144,7 +205,9 @@ function InformationPage() {
                   <div className="ca3h1 ca3Orange">Status:</div>
                 </Col>
                 <Col sm={8}>
-                  <div className="ca3P ca3White">{nextlaunch.results[index].status.name}</div>
+                  <div className="ca3P ca3White">
+                    {nextlaunch.results[index].status.name}
+                  </div>
                 </Col>
               </Row>
 
@@ -162,7 +225,9 @@ function InformationPage() {
                   <div className="ca3h1 ca3Orange">Launch Provider:</div>
                 </Col>
                 <Col sm={8}>
-                  <div className="ca3P ca3White">{nextlaunch.results[index].launch_service_provider.name}</div>
+                  <div className="ca3P ca3White">
+                    {nextlaunch.results[index].launch_service_provider.name}
+                  </div>
                 </Col>
               </Row>
               <Row>
@@ -170,13 +235,19 @@ function InformationPage() {
                   <div className="ca3h1 ca3Orange">Provider Type:</div>
                 </Col>
                 <Col sm={8}>
-                  <div className="ca3P ca3White">{nextlaunch.results[index].launch_service_provider.type}</div>
+                  <div className="ca3P ca3White">
+                    {nextlaunch.results[index].launch_service_provider.type}
+                  </div>
                 </Col>
               </Row>
 
               <Row>
-                <Col sm={4}><div className="ca3P">&nbsp;</div></Col>
-                <Col sm={8}><div className="ca3P ca3White">&nbsp;</div></Col>
+                <Col sm={4}>
+                  <div className="ca3P">&nbsp;</div>
+                </Col>
+                <Col sm={8}>
+                  <div className="ca3P ca3White">&nbsp;</div>
+                </Col>
               </Row>
 
               <Row>
@@ -184,7 +255,11 @@ function InformationPage() {
                   <div className="ca3h1 ca3Orange">Mission Name:</div>
                 </Col>
                 <Col sm={8}>
-                  <div className="ca3P ca3White">{(nextlaunch.results[index].mission !== null) ? nextlaunch.results[index].mission.name : 'Not available'}</div>
+                  <div className="ca3P ca3White">
+                    {nextlaunch.results[index].mission !== null
+                      ? nextlaunch.results[index].mission.name
+                      : "Not available"}
+                  </div>
                 </Col>
               </Row>
               <Row>
@@ -192,7 +267,11 @@ function InformationPage() {
                   <div className="ca3h1 ca3Orange">Mission Type:</div>
                 </Col>
                 <Col sm={8}>
-                  <div className="ca3P ca3White">{(nextlaunch.results[index].mission !== null) ? nextlaunch.results[index].mission.type : 'Not available'}</div>
+                  <div className="ca3P ca3White">
+                    {nextlaunch.results[index].mission !== null
+                      ? nextlaunch.results[index].mission.type
+                      : "Not available"}
+                  </div>
                 </Col>
               </Row>
               <Row>
@@ -200,13 +279,21 @@ function InformationPage() {
                   <div className="ca3h1 ca3Orange">Mission Details:</div>
                 </Col>
                 <Col sm={8}>
-                  <div className="ca3P ca3White">{(nextlaunch.results[index].mission !== null) ? nextlaunch.results[index].mission.description : 'Not available'}</div>
+                  <div className="ca3P ca3White">
+                    {nextlaunch.results[index].mission !== null
+                      ? nextlaunch.results[index].mission.description
+                      : "Not available"}
+                  </div>
                 </Col>
               </Row>
 
               <Row>
-                <Col sm={4}><div className="ca3P">&nbsp;</div></Col>
-                <Col sm={8}><div className="ca3P ca3White">&nbsp;</div></Col>
+                <Col sm={4}>
+                  <div className="ca3P">&nbsp;</div>
+                </Col>
+                <Col sm={8}>
+                  <div className="ca3P ca3White">&nbsp;</div>
+                </Col>
               </Row>
 
               <Row>
@@ -214,22 +301,34 @@ function InformationPage() {
                   <div className="ca3h1 ca3Orange">Rocket:</div>
                 </Col>
                 <Col sm={8}>
-                  <div className="ca3P ca3White">{nextlaunch.results[index].rocket.configuration.full_name}</div>
+                  <div className="ca3P ca3White">
+                    {nextlaunch.results[index].rocket.configuration.full_name}
+                  </div>
                 </Col>
               </Row>
               <Row>
                 <Col>
                   <br />
-                  <img src={nextlaunch.results[index].image} className="imgRocket"></img>
+                  <img
+                    src={nextlaunch.results[index].image}
+                    className="imgRocket"
+                  ></img>
                   <br />
                   <br />
                 </Col>
               </Row>
-
+              <Row>
+                <Col>
+                 <Comments 
+                 rocketID={nextlaunch.results[index].id}/>
+                </Col>
+              </Row>
             </Container>
           </div>
         </div>
-      ) : ('')}
+      ) : (
+        ""
+      )}
     </div>
   );
 }
