ca76ccbf4fdf19fb91b3f06e6c9e6a437d7b9f10
Mick <grevmivlos@gmail.com>
2020-12-11 14:01:26 +0100

    fixed connection string to use cloud API

--- src/settings.js
+++ src/settings.js
@@ -1,8 +1,2 @@
-export const URL = "http://localhost:8080/CA3_Boilerplate";
-//export const URLJokes = "http://localhost:8080/CA3_Boilerplate/api/jokes";
-//export const URLScrapeSequential = "http://localhost:8080/CA3_Boilerplate/api/scrape/sequential";
-//export const URLScrapeParallel = "http://localhost:8080/CA3_Boilerplate/api/scrape/parallel";
-//export const URLJokes = "https://cphper.dk/CA3_Boilerplate_Backend/api/jokes";
-//export const URLScrapeSequential = "https://cphper.dk/CA3_Boilerplate_Backend/api/scrape/sequential";
-//export const URLScrapeParallel = "https://cphper.dk/CA3_Boilerplate_Backend/api/scrape/parallel";
-//export const URL = "https://micklarsen.com/3_sem_eksamensprojekt";
+//export const URL = "http://localhost:8080/CA3_Boilerplate";
+export const URL = "https://micklarsen.com/3_sem_eksamensprojekt";
